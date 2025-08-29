1cb8a867057beb7f9d922d7555a667c835132e62
Daniel Talbot-Bowe <dtbdesign@Mac.hsd1.tn.comcast.net>
2025-08-28 19:05:36 -0500

    Add August 28th CCIP data to data service

--- src/services/ccipDataService.ts
+++ src/services/ccipDataService.ts
@@ -123,7 +123,8 @@ class CCIPDataService {
         '08-24-2025 CCIP.csv',
         '08-25-2025 CCIP.csv',
         '08-26-2025 CCIP.csv',
-        '08-27-2025 CCIP.csv'
+        '08-27-2025 CCIP.csv',
+        '8-28-2025 CCIP.csv'
       ];
 
       const loadPromises = csvFiles.map(async (filename) => {
