8141eacd3abf75a89b5f477d1c4059f2aa5f8452
Daniel Talbot-Bowe <dtbdesign@Daniels-MacBook-Pro.local>
2025-09-01 19:56:42 -0500

    Add 09-01

--- src/services/ccipDataService.ts
+++ src/services/ccipDataService.ts
@@ -127,7 +127,8 @@ class CCIPDataService {
         '08-28-2025 CCIP.csv',
         '08-29-2025 CCIP.csv',
         '08-30-2025 CCIP.csv',
-        '08-31-2025 CCIP.csv'
+        '08-31-2025 CCIP.csv',
+        '09-1-2025 CCIP.csv'
       ];
 
       const loadPromises = csvFiles.map(async (filename) => {
