e5546eee5e8e4d466210c8dcb48cff8db7bd56b7
Daniel Talbot-Bowe <dtbdesign@Mac.hsd1.tn.comcast.net>
2025-09-18 19:07:43 -0500

    september 18th

--- src/services/ccipDataService.ts
+++ src/services/ccipDataService.ts
@@ -144,7 +144,8 @@ class CCIPDataService {
         "09-14-2025 CCIP.csv",
         "09-15-2025 CCIP.csv",
         "09-16-2025 CCIP.csv",
-        "09-17-2025 CCIP.csv"
+        "09-17-2025 CCIP.csv",
+        "09-18-2025 CCIP.csv"
       ];
 
       const loadPromises = csvFiles.map(async (filename) => {
